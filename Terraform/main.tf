13f326ddbf1cee63e2ff3cec78d65e30c6b47789
MicaBelle <micabelleggia@gmail.com>
2024-08-27 00:02:04 -0300

    Terraform base commits

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,29 +1,113 @@
+##CONFIGURACIONES DE VPC Y SUBNETEO##
 
-resource "aws-instance" "test" { 
-    ami = 
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-    key_name = vars.key_name
-    vpc_security_groups-ids = [""] 
-    tags {
-        Name = "test1"
+#Creamos la VPC donde hostearemos todo 
+resource "aws_vpc" "gorra_vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = { 
+        Name = "gorra-vpc"
     }
 }
+
+#Creamos la Public Subnet
+resource "aws_subnet" "gorra_public_subnet"{ 
+    vpc_id = aws_vpc.gorra_vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = var.ZONE1   
+    map_public_ip_on_launch = true
+    tags = { 
+        Name = "gorra-public-subnet"
+    }
+}
+
+#Creamos el Instranet Gateway
+resource "aws_internet_gateway" "gorra_igw" {
+    vpc_id = aws_vpc.gorra_vpc.id
+    tags = { 
+        Name= "gorra-igw"
+    }
+}
+
+#Creamos la Route Table 
+resource "aws_route_table" "gorra_route_table" { 
+    vpc_id = aws_vpc.gorra_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gorra_igw.id
+    }
+    tags = { 
+        Name = "gorra-route-table"
+    }
+}
+
+#Associamos la Route Table con la Subnet
+resource "aws_route_table_association" "gorra_route_table_assoc" {
+    subnet_id = aws_subnet.gorra_public_subnet.id
+    route_table_id = aws_route_table.gorra_route_table.id
+}
+
+##CREACION DEL BUCKET##
 
 resource "aws_s3_bucket" "gorra_bucket" {
     bucket = var.bucket_name
     acl = "private"
 }
 
+##CREACION Y CONFIGURACION DE SG##
+
+resource "aws_security_group" "gorra_sg" {
+    name = "gorra-sg"
+    description = "Security group for web app Gorra"
+    vpc_id = aws_vpc.gorra_vpc.id
+
+#Inbound rules
+ingress { 
+    description = "Allow SSH from a specific IP address"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["$ {var.my_ip}/32"] 
+}
+
+ingress {
+    description = "Allow HTTP from anywhere"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+}
+
+    ingress {
+        description = "Allow HTTPS from anyehere"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+#Outbound rules
+egress {
+    description = "Allow all outbound traffic"
+    from_port = 0
+    to_port = 0 
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+tags = {
+    Name = "gorra-sg"
+}
+
+## CREACION DE LA EC2##
+
 resource "aws_instance" "test_inst" { 
-    ami = var.AMI[var.REGION]
+    ami = var.AMIS[var.REGION]
     instance_type = "t2.micro"
     availability_zone = var.ZONE1
-    key_name = "test_key"
-    vpc_security_groups_ids = ["<sg_id>"]
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.gorra_sg.id] 
+    subnet_id                   = aws_subnet.gorra_public_subnet.id
 
     tags {
-        name = "Test_instance"
-        project = "GORRA"
+        Name = "Test_instance"
+        Project = "GORRA"
     }
 }
--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,22 +1,39 @@
-variable REGION {
+variable "REGION" {
     default = "us-east-1"
 }
 
-variable ZONE1 {
-    default "us-east-1"
+variable "ZONE1" {
+    default = "us-east-1a"
 }
 
-variable AMIS { 
-    type = map
+variable "AMIS" { 
+    type    = map(string)
     default = { 
-        us-east-2 = "<ami_id>" #completar
-        us-east-1 = "<ami_id>" #completar
-
+        us-east-2 = "ami-0c55b159cbfafe1f0" 
+        us-east-1 = "ami-0c55b159cbfafe1f0"
     }
 }
+
 
 variable "bucket_name" { 
     description = "Bucket del sistema Gorra"
     type = string 
     default = "gorra_bucket_2024"
+}
+
+variable "key_name" {
+    description = "Key pair name for EC2 instance"
+    type = string 
+    default = "test_key" #reemplazar con un key_name 
+}
+
+variable "my_ip" {
+    description = "My personal IP, change if necessary"
+    default = "190.195.209.92"
+}
+
+variable "vpc_id" { 
+    description: "The ID of the VPC where resources will be created"
+    type = string
+    default = "aws_vpc.gorra_vpc.id" 
 }
