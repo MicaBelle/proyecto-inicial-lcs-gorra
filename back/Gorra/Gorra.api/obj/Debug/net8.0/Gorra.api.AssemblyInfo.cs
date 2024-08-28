9507c8659b8d4f1e7aff453bb44c2a362e7a0209
Francisco Florio <dfrancisco25@hotmail.com>
2024-08-28 00:42:04 -0300

    Agregado etiqueta outputtype

--- back/Gorra/Gorra.api/obj/Debug/net8.0/Gorra.api.AssemblyInfo.cs
+++ back/Gorra/Gorra.api/obj/Debug/net8.0/Gorra.api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gorra.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df21862c561760e009cdde85febc38c217619e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce2f39e586ab7607ee90d4877cdaaa86b400a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gorra.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gorra.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
