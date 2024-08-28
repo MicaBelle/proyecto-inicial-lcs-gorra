f31a903d7f27f39ed914e4ba6673ed656fa57aa3
Francisco Florio <dfrancisco25@hotmail.com>
2024-08-28 00:52:26 -0300

    WSD

--- back/Gorra/Gorra.api/obj/Debug/net8.0/Gorra.api.AssemblyInfo.cs
+++ back/Gorra/Gorra.api/obj/Debug/net8.0/Gorra.api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gorra.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce2f39e586ab7607ee90d4877cdaaa86b400a20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc585eaed979e8fa12c117b9821bc765a348946")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gorra.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gorra.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
