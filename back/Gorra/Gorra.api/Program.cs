ea4a243e67424018b5ecba04e9804456e09c0f28
Tomas Ledesma <tomasld13@gmail.com>
2024-08-28 23:05:52 -0300

    Revert "Merge branch 'main' of https://github.com/sgs31/proyecto-inicial-lcs-gorra"
    This reverts commit b411b92a1f012701a9c70300574318e7384709fa, reversing
    changes made to fc4773fe37b63ce1acb533c0db1d999bbf69329e.

--- back/Gorra/Gorra.api/Program.cs
+++ back/Gorra/Gorra.api/Program.cs
@@ -23,3 +23,8 @@ app.UseHttpsRedirection();
 
 
 app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
