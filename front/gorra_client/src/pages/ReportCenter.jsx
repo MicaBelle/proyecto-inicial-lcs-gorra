c9beb02ef94a4d32cf91e8d9f0fb25f309ea2817
Agustin Aguirre <agustinaguirre3195@gmail.com>
2024-09-03 18:01:41 -0300

    add: cambios en home y reporte

--- front/gorra_client/src/pages/ReportCenter.jsx
+++ front/gorra_client/src/pages/ReportCenter.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LocationMarker from '../components/locationMarker/LocationMarker';
 import GeoRef from '../services/geoRef';
@@ -26,49 +26,48 @@ export default function ReportCenter() {
   }, []);
 
   async function executeGeoref(){
-    const data = await GeoRef.getLocationForCoords(location[0], location[1])
-    console.log(data)
+    const data = await GeoRef.getLocationForCoords(location[0], location[1]);
+    console.log(data);
   }
 
   return (
     <Container fluid className="p-3">
-      <h1 className="text-center">Reportar robo</h1>
-      {error ? (
-        <p className="text-danger text-center">Debe darle permisos a la app para saber su ubicación.</p>
-      ) : location ? (
-        <>
-          <Row className="justify-content-center">
-            <Col xs={12} md={8}>
-              <div style={{ height: '200px', width: '100%' }}>
-                <MapContainer center={location} zoom={13} style={{ height: '100%', width: '100%' }}>
-                  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                  <Marker position={location}>
-                    <Popup>Estás aquí</Popup>
-                  </Marker>
-                  <LocationMarker setLocation={setLocation} />
-                </MapContainer>
-              </div>
-            </Col>
-          </Row>
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Card>
+            <Card.Header as="h1" className="text-center">Reportar robo</Card.Header>
+            <Card.Body>
+              {error ? (
+                <p className="text-danger text-center">Debe darle permisos a la app para saber su ubicación.</p>
+              ) : location ? (
+                <>
+                  <div style={{ height: '40vh', width: '100%'}}>
+                    <MapContainer center={location} zoom={16} style={{ height: '100%', width: '100%' }}>
+                      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                      <Marker position={location}>
+                        <Popup>Estás aquí</Popup>
+                      </Marker>
+                      <LocationMarker setLocation={setLocation} />
+                    </MapContainer>
+                  </div>
 
-          <Row className="justify-content-center mt-3">
-            <Col xs={12} md={8}>
-              <h3>Detalles del acontecimiento</h3>
-              <Form>
-                <Form.Group controlId="incidentDetails">
-                  <Form.Control type="text" placeholder="Describe el incidente" />
-                </Form.Group>
-                <Button variant="primary" onClick={executeGeoref} block>
-                  Reportar robo
-                </Button>
-              </Form>
-              <p className="mt-2">Coordenadas: {location.join(', ')}</p>
-            </Col>
-          </Row>
-        </>
-      ) : (
-        <p className="text-center">Obteniendo ubicación...</p>
-      )}
+                  <h3 className="mt-3">Detalles del acontecimiento</h3>
+                  <Form>
+                    <Form.Group controlId="incidentDetails">
+                      <Form.Control type="text" placeholder="Describe el incidente" />
+                    </Form.Group>
+                    <Button variant="primary" onClick={executeGeoref} block>
+                      Reportar robo
+                    </Button>
+                  </Form>
+                </>
+              ) : (
+                <p className="text-center">Obteniendo ubicación...</p>
+              )}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 }
