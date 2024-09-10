7e45a4fb688f192e0d5dea05e86186dae8efdd31
Tomas Ledesma <tomasld13@gmail.com>
2024-09-10 20:12:00 -0300

    fix: botttomNavigation
    se esconde la barra de navegacion si el usuario no esta logueado

--- front/gorra_client/src/components/bottomNavigation/bottomNavigation.jsx
+++ front/gorra_client/src/components/bottomNavigation/bottomNavigation.jsx
@@ -1,3 +1,4 @@
+// SimpleBottomNavigation.js
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
@@ -5,39 +6,47 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import FormatListNumberedRtlIcon from '@mui/icons-material/FormatListNumberedRtl';
 import AddIcon from '@mui/icons-material/Add';
 import HomeIcon from '@mui/icons-material/Home';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
+  const location = useLocation();
+  const [autenticado, setAutenticado] = React.useState(localStorage.getItem('user'))
+
+  React.useEffect(() => {
+    setAutenticado(localStorage.getItem('user'))
+  },[location])
 
   return (
-    <Box sx={{ width: "100%"}}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction
-        label="Inicio"
-        icon={<HomeIcon />}
-        component={Link}
-        to="/home"
-      />
-      <BottomNavigationAction
-        label="Denunciar robo"
-        icon={<AddIcon />}
-        component={Link}
-        to="/report"
-      />
-      <BottomNavigationAction
-        label="Mis denuncias"
-        icon={<FormatListNumberedRtlIcon />}
-        component={Link}
-        to="/my-reports"
-      />
-      </BottomNavigation>
+    <Box sx={{ width: "100%" }}>
+      {autenticado != null && (
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            label="Inicio"
+            icon={<HomeIcon />}
+            component={Link}
+            to="/home"
+          />
+          <BottomNavigationAction
+            label="Denunciar robo"
+            icon={<AddIcon />}
+            component={Link}
+            to="/report"
+          />
+          <BottomNavigationAction
+            label="Mis denuncias"
+            icon={<FormatListNumberedRtlIcon />}
+            component={Link}
+            to="/my-reports"
+          />
+        </BottomNavigation>
+      )}
     </Box>
   );
 }
